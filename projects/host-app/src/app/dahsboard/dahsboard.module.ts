9a53cedb0afed22bbd24ebc0f9c7acfd21c0575a
Alvaro3131 <73666415+Alvaro3131@users.noreply.github.com>
2023-09-13 17:46:48 -0500

    finish login

--- projects/host-app/src/app/dahsboard/dahsboard.module.ts
+++ projects/host-app/src/app/dahsboard/dahsboard.module.ts
@@ -2,9 +2,10 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { DahsboardComponent } from './dahsboard.component';
+import { DashboardRoutingModule } from './dashboard.routes';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, DashboardRoutingModule],
   declarations: [DahsboardComponent],
   exports: [DahsboardComponent],
 })
