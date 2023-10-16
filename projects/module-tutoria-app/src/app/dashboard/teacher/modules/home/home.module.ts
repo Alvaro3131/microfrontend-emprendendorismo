eaa276552fca61170bc031cb320d2815306e96ac
josueprogram2003 <73667027+josueprogram2003@users.noreply.github.com>
2023-10-16 11:59:05 -0500

    v4

--- projects/module-tutoria-app/src/app/dashboard/teacher/modules/home/home.module.ts
+++ projects/module-tutoria-app/src/app/dashboard/teacher/modules/home/home.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
-import { SharedModule } from 'projects/module-tutoria-app/src/app/shared/shared.module';
 import { HomeComponent } from './pages/home/home.component';
+import { SharedModule } from 'projects/module-tutoria-app/src/app/shared/shared.module';
 
 
 @NgModule({
