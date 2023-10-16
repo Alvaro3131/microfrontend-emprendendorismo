6f95317fd59bd599c1d9f5cc04bdd60bc9600f18
josueprogram2003 <73667027+josueprogram2003@users.noreply.github.com>
2023-10-16 11:18:32 -0500

    v3

--- projects/module-tutoria-app/src/app/dashboard/dashboard.module.ts
+++ projects/module-tutoria-app/src/app/dashboard/dashboard.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DahsboardComponent } from './dashboard.component';
 import { ListActivitiesComponent } from './components/list-activities/list-activities.component';
 import { RegistrarAsistenciaComponent } from './components/registrar-asistencia/registrar-asistencia.component';
-import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
