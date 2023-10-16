c18d5f9a027301073b979c736b79a3beaac8571e
josueprogram2003 <73667027+josueprogram2003@users.noreply.github.com>
2023-10-16 11:53:27 -0500

    correciones

--- projects/module-tutoria-app/src/app/dashboard/teacher/modules/home/home.module.ts
+++ projects/module-tutoria-app/src/app/dashboard/teacher/modules/home/home.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { HomeRoutingModule } from './home-routing.module';
-import { HomeComponent } from './pages/home/home.component';
 import { SharedModule } from 'projects/module-tutoria-app/src/app/shared/shared.module';
+import { HomeComponent } from './pages/home/home.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent
-
   ],
   imports: [
     CommonModule,
