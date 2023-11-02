5a8381b04225dcd8fa10f900b51f789bb14e6557
josueprogram2003 <73667027+josueprogram2003@users.noreply.github.com>
2023-11-02 15:45:27 -0500

    v1

---
+++ projects/host-app/src/app/dahsboard/teacher/header-teacher/header-teacher.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-teacher',
+  templateUrl: './header-teacher.component.html',
+  styleUrls: ['./header-teacher.component.scss']
+})
+export class HeaderTeacherComponent {
+
+}
--- projects/host-app/src/app/dahsboard/teacher/teacher.module.ts
+++ projects/host-app/src/app/dahsboard/teacher/teacher.module.ts
@@ -2,10 +2,13 @@ import { CommonModule } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA, NgModule } from '@angular/core';
 import { TeacherComponent } from './teacher.component';
 import { TeacherRoutingModule } from './teacher-routing.module';
+import { HeaderTeacherComponent } from './header-teacher/header-teacher.component';
+import { ProyeccionComponent } from './modules/proyeccion/proyeccion.component';
+import { TutoriaComponent } from './modules/tutoria/tutoria.component';
 
 @NgModule({
   imports: [CommonModule,TeacherRoutingModule],
-  declarations: [TeacherComponent],
+  declarations: [TeacherComponent, HeaderTeacherComponent, ProyeccionComponent, TutoriaComponent],
   schemas: [
     CUSTOM_ELEMENTS_SCHEMA,
     NO_ERRORS_SCHEMA
