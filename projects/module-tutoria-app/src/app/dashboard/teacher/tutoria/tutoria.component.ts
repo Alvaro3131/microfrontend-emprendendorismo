29166045cc519713ec9e266d44d5770a521efc3a
RenzoBTL30 <90786322+RenzoBTL30@users.noreply.github.com>
2023-11-09 16:41:29 -0500

    cambios en module-tutoria-app

---
+++ projects/module-tutoria-app/src/app/dashboard/teacher/tutoria/tutoria.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tutoria',
+  templateUrl: './tutoria.component.html',
+  styleUrls: ['./tutoria.component.scss']
+})
+export class TutoriaComponent {
+
+}
